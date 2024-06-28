ae2669d7c59c3353d6e283fcbae81b59f94e9c27
JohnCodes <engrlevijohn@gmail.com>
2024-06-28 11:51:48 +0100

    adgjusted responsiveness

--- components/About.tsx
+++ components/About.tsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <>
       <div className="p-5 mt-20">
-        <div className="mx-[80px] mt-5 text-white">
+        <div className="mx-[80px] max-lg:mx-5 mt-5 text-white">
           <h1 className="font-main text-2xl text-center">How it Works</h1>
-          <div className="lg:grid grid-cols-12 gap-5 items-center">
+          <div className="grid lg:grid-cols-12 gap-5 items-center">
             <div className="col-span-6">
-              <p className="text-right">
+              <p className="text-right max-sm:text-left">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                 accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
                 quae ab illo inventore veritatis et quasi architecto beatae
