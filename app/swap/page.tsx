f0fafb7db4869ee6e7515cc09b3705b8790256cd
JohnCodes <engrlevijohn@gmail.com>
2024-07-01 17:50:34 +0100

    fix: Swap UI

--- app/swap/page.tsx
+++ app/swap/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <>
       <h1 className="text-white text-center text-2xl font-main">Swap Tokens</h1>
+      <h1 className="text-white text-center text-2xl font-main">Bond</h1>
       {/* <TradeSwap /> */}
     </>
   );
